2f384b0b041662337b2999c4005b7b7af4f38150
Nirmal Kumar Gopalakrishnan <nirmalceg@gmail.com>
2020-05-15 19:57:44 +0530

    Added Controller for User

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"jwt-course/driver"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/subosito/gotenv"
 )
 
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
+func logError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 func main() {
-	fmt.Println("Hello World")
+	db = driver.ConnectDB()
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", controller.Signup(db)).Methods("POST")
+	router.HandleFunc("/login", controller.Login(db)).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
